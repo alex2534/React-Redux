f97fbf0c08d1654dcca83b0889173cd2151f06eb
alex2534 <alex2534@outlook.com>
2022-04-15 19:41:23 -0300

    Attaching Payloads to Actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,55 @@
-import classes from './Counter.module.css';
+/** @format */
+
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+	//we don't have to give any argumento this useDispatch
+	//it gives as a usedispatch function so we can call and it will dispatch a action
+	//agains the redux store
+	const dispatch = useDispatch();
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+	//we have to pass a function to useSelector the function
+	//will recieve the state from the redux
+	//the parte we want to extract from the redux store
+	const counter = useSelector((state) => state.counter);
+
+	const incrementHandler = () => {
+		//an action is an object as an argument
+		dispatch({
+			type: "increment",
+		});
+	};
+
+	const increasetHandler = () => {
+		dispatch({
+			type: "increase",
+			amount: 10,
+		});
+	};
+
+	const decrementHandler = () => {
+		//an action is an object as an argument
+		dispatch({
+			type: "decrement",
+		});
+	};
+
+	const toggleCounterHandler = () => {};
+
+	return (
+		<main className={classes.counter}>
+			<h1>Redux Counter</h1>
+			<div className={classes.value}>{counter}</div>
+			<div>
+				<button onClick={incrementHandler}>increment</button>
+				<button onClick={increasetHandler}>Increase by 10</button>
+				<button onClick={decrementHandler}>Decrement</button>
+			</div>
+			<button onClick={toggleCounterHandler}>Toggle Counter</button>
+		</main>
+	);
 };
 
 export default Counter;
--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,20 @@
 import react, { createStore } from "redux";
 
 const counterReducer = (state = { counter: 0 }, action) => {
+	//an action return an object of any type
 	if (action.type === "increment") {
 		return {
 			counter: state.counter + 1,
 		};
 	}
 
+	//an action return an object of any type
+	if (action.type === "increase") {
+		return {
+			counter: state.counter + action.amount,
+		};
+	}
+	//an action return an object of any type
 	if (action.type === "decrement") {
 		return {
 			counter: state.counter - 1,
